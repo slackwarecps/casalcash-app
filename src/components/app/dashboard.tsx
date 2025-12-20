9140b0637413ea793a6c125bf8fc9566ff3abe86
Fabio Pereira <fabio.alvaro@gmail.com>
2025-12-20 00:53:04 +0000

    Na home adicione mais um card  que mostre o Texto "Divisao por dois: " {

--- src/components/app/dashboard.tsx
+++ src/components/app/dashboard.tsx
@@ -181,13 +181,22 @@ export default function Dashboard({ expenses, preCredits, loans, currentUser, se
       </CardHeader>
       <CardContent className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-4 items-start">
         <div className="grid grid-cols-2 md:grid-cols-4 col-span-1 md:col-span-3 lg:col-span-1 gap-4">
-             <Card className="bg-background/70 col-span-2 md:col-span-4">
+             <Card className="bg-background/70 col-span-2">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">Total do Mês</CardTitle>
                     <CreditCard className="h-4 w-4 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
                     <div className="text-2xl font-bold">{formatCurrency(totalExpenses)}</div>
+                </CardContent>
+            </Card>
+             <Card className="bg-background/70 col-span-2">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Divisão por Dois</CardTitle>
+                    <Users className="h-4 w-4 text-muted-foreground" />
+                </CardHeader>
+                <CardContent>
+                    <div className="text-2xl font-bold">{formatCurrency(totalExpenses / 2)}</div>
                 </CardContent>
             </Card>
             
